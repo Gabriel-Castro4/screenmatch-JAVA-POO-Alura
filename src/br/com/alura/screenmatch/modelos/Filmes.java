479ecd9b7fb9553135ee22bf9c913097107621b6
Gabriel <gabriel.castro@castrom.net>
2026-05-15 17:03:49 -0300

    Passando a paternidade da classe Filmes para a classe Titulo e Series e Filmes estão herdando dela

--- src/br/com/alura/screenmatch/modelos/Filmes.java
+++ src/br/com/alura/screenmatch/modelos/Filmes.java
@@ -1,100 +1,23 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filmes {
+public class Filmes extends Titulo {
 
     //Atributos
-     private String nome;
-     private int anoDeLancamento;
-     private boolean incluidoNoPlano;
-     private double somaDasAvaliacoes;
-     private int totalDeAvaliacoes;
-     private int duracaoEmMinutos;
+    private String diretor;
+
 
 
      //Getters and Setters
-    public String getNome() {
-        return nome;
+
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    //Não quero que o usuário tenha acesso a soma das avaliações
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    //Não quero que o usuário tenha acesso ao set do total avaliações
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-
-    //COMMIT 4
-    /*Como agora foi criado um package para guardar a classse filmes, é necessário deixar explicito que os métodos são públicos*/
 
     //Métodos
-   public void exibeFichaTecnica(){
-        System.out.println("Nome do Filme: " + nome);
-        System.out.println("Ano de Lançamento: " + anoDeLancamento);
-        System.out.println("Duração em minutos: " + duracaoEmMinutos);
-        //COMMIT 2
-        System.out.printf("Média Global: %.1f", mediaDasAvaliacoes());
-    }
-
-    public void avalia(double nota) {
-
-        /*Antes eu estava usando apenas o "if", mas eu estava achando
-         * fora dos padrões aparecer um aviso em forma de sout na tela de runtime,
-         * por isso decidi usar o Exception*/
-        //COMMIT 3
-        /*Novamente uma remoção de verbosidade, dessa vez no try catch dentro do else, que não precisva, era apenas fechar a condição e dizer oque ela deveria ser feita caso fosse falsa*/
-        if (nota > 10 || nota < 0) {
-            throw new RuntimeException("Valor de nota inválido, Insira um valor entre 0 e 10");
-        }
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-
-    }
-
-    //COMMIT 2
-    /*Para diminuir a quantidade de linhas, percebi q o métedo mediaGlobal era redundante e então decidi descobrir outra forma de trabalhar esse método e veja como ficou mais simples e menos verboso:
-        * double mediaDasAvaliacoes(){
-        double media = somaDasAvaliacoes / totalDeAvaliacoes;
-        return media;
-    }
-
-    void mediaGlobal(){
-        System.out.printf("%.1f", mediaDasAvaliacoes());
-    }
-}
-*   Para: */
-    public double mediaDasAvaliacoes(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-
-    }
 
 }
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,78 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+
+    //Atributos
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    //Não quero que o usuário tenha acesso a soma das avaliações
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    //Não quero que o usuário tenha acesso ao set do total avaliações
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+
+    //Métodos
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+        System.out.printf("Média Global: %.1f", mediaDasAvaliacoes());
+    }
+
+    public void avalia(double nota) {
+
+        if (nota > 10 || nota < 0) {
+            throw new RuntimeException("Valor de nota inválido, Insira um valor entre 0 e 10");
+        }
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+
+    }
+
+    public double mediaDasAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+
+    }
+
+}
